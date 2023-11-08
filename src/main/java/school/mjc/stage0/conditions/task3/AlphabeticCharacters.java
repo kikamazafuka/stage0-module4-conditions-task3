57cd20f8bc975d5d0495d09deb8e2c1eec224571
Artur Semenas <uncelfuka@gmail.com>
2023-11-08 23:24:00 +0100

    Module #4.1. Task #3. Multiple Conditions

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (isVowel(character)){
+            System.out.println("Vowel");
+        } else if (((character>96 && character<123) || (character>64 && character<91)) && !isVowel(character)){
+            System.out.println("Consonant");
+        } else System.out.println("wrong alphabet!");
+    }
+
+    private static boolean isVowel (char character){
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'
+                || character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U'){
+            return true;
+        }
+        return false;
     }
 }
